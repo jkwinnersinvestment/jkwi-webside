95866060c534aa5fbcd93d344f0b28043c9fc4f0
jkwinnersinvestment <info@jkwinnersinvestment.co.za>
2026-07-27 23:10:56 +0200

    added market

--- js/script.js
+++ js/script.js
@@ -51,3 +51,59 @@ async function loadWTI() {
 loadWTI();
 
 setInterval(loadWTI, 3600000);
+window.addEventListener("scroll", function(){
+
+document.querySelector(".navbar").classList.toggle("scrolled", window.scrollY > 50);
+
+});
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+menuToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".dropdown > a").forEach(item => {
+
+    item.addEventListener("click", function(e){
+
+        if(window.innerWidth <= 992){
+
+            e.preventDefault();
+
+            this.parentElement.classList.toggle("open");
+
+        }
+
+    });
+
+});
+const API_KEY = 2ef03a84d72f46b498defbe0d6aed423
+
+async function getPrice(symbol, elementId) {
+    try {
+        const response = await fetch(
+            `https://api.twelvedata.com/price?symbol=${symbol}&apikey=${API_KEY}`
+        );
+
+        const data = await response.json();
+
+        if (data.price) {
+            document.getElementById(elementId).textContent =
+                "$" + parseFloat(data.price).toFixed(2);
+        } else {
+            document.getElementById(elementId).textContent = "N/A";
+            console.log(data);
+        }
+
+    } catch (error) {
+        document.getElementById(elementId).textContent = "Error";
+        console.error(error);
+    }
+}
+
+// Examples
+getPrice("XAU/USD", "gold");
+getPrice("XAG/USD", "silver");
